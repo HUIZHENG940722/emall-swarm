caa811d210cb210cc196d6a46df15c6894d1af11
HUIZHNEG940722 <zhenghui940722@outlook.com>
2021-11-27 22:03:23 +0800

    swagger-ui配置

--- mall-admin/src/main/java/com/ethan/mall/admin/MallAdminApplication.java
+++ mall-admin/src/main/java/com/ethan/mall/admin/MallAdminApplication.java
@@ -2,8 +2,10 @@ package com.ethan.mall.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.ethan.mall"})
 public class MallAdminApplication {
 
     public static void main(String[] args) {
---
+++ mall-admin/src/main/java/com/ethan/mall/admin/controller/UmsAdminController.java
@@ -0,0 +1,17 @@
+package com.ethan.mall.admin.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ethan
+ * @Date 7:25 下午 2021/11/27
+ * @Description 后台用户管理
+ */
+@RestController
+@RequestMapping(value = "/admin")
+@Api(tags = "UmsAdminController")
+public class UmsAdminController {
+
+}
