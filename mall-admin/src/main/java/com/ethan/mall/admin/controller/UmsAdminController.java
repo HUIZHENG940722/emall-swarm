1b24e447a26c1caf66529e25eec386fbd61600b0
HUIZHNEG940722 <zhenghui940722@outlook.com>
2021-11-28 15:07:32 +0800

    整合参数校验，采用Spring boot自带的参数校验

--- mall-admin/src/main/java/com/ethan/mall/admin/controller/UmsAdminController.java
+++ mall-admin/src/main/java/com/ethan/mall/admin/controller/UmsAdminController.java
@@ -7,6 +7,7 @@ import com.ethan.mall.model.UmsAdmin;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,7 @@ public class UmsAdminController {
 
     @ApiOperation(value = "注册用户")
     @PostMapping(value = "/register")
-    public CommonData<UmsAdmin> register(@RequestBody UmsAdminRegisterParam adminRegisterParam) {
+    public CommonData<UmsAdmin> register(@Validated @RequestBody UmsAdminRegisterParam adminRegisterParam) {
         UmsAdmin admin = adminService.register(adminRegisterParam);
         if (admin == null) {
             return CommonData.failed();
--- mall-admin/src/main/java/com/ethan/mall/admin/domain/UmsAdminRegisterParam.java
+++ mall-admin/src/main/java/com/ethan/mall/admin/domain/UmsAdminRegisterParam.java
@@ -3,6 +3,9 @@ package com.ethan.mall.admin.domain;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author ethan
  * @Date 11:45 上午 2021/11/28
@@ -11,11 +14,14 @@ import lombok.Data;
 @Data
 public class UmsAdminRegisterParam {
     @ApiModelProperty(value = "用户名", required = true)
+    @NotBlank
     private String username;
     @ApiModelProperty(value = "密码", required = true)
+    @NotBlank
     private String password;
     @ApiModelProperty(value = "用户头像")
     private String icon;
+    @Email
     @ApiModelProperty(value = "邮箱")
     private String email;
     @ApiModelProperty(value = "用户昵称")
