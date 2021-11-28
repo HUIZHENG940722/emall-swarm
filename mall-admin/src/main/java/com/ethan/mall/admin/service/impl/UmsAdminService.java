2bff0686e99af24c383a1f80ba5125542ae7d8e7
HUIZHNEG940722 <zhenghui940722@outlook.com>
2021-11-28 15:01:51 +0800

    修复后台用户注册创建时间未初始化等小瑕疵

--- mall-admin/src/main/java/com/ethan/mall/admin/service/impl/UmsAdminService.java
+++ mall-admin/src/main/java/com/ethan/mall/admin/service/impl/UmsAdminService.java
@@ -1,14 +1,19 @@
 package com.ethan.mall.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.StrUtil;
 import com.ethan.mall.admin.domain.UmsAdminRegisterParam;
 import com.ethan.mall.admin.service.IUmsAdminService;
 import com.ethan.mall.common.exception.Asserts;
 import com.ethan.mall.mapper.UmsAdminMapper;
 import com.ethan.mall.model.UmsAdmin;
+import com.ethan.mall.model.UmsAdminExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author ethan
@@ -27,9 +32,22 @@ public class UmsAdminService implements IUmsAdminService {
                 || StrUtil.isBlank(adminRegisterParam.getPassword())) {
             Asserts.fail("用户名或密码不能为空");
         }
+        // 1.2 校验用户名是否存在
+        UmsAdminExample adminExample = new UmsAdminExample();
+        adminExample.createCriteria().andUsernameEqualTo(adminRegisterParam.getUsername());
+        List<UmsAdmin> umsAdmins = adminMapper.selectByExample(adminExample);
+        if (CollUtil.isNotEmpty(umsAdmins)) {
+            Asserts.fail("用户名已存在");
+        }
         // 2 插入后台用户
+        // 2.1 赋值基础数据
         UmsAdmin admin = new UmsAdmin();
+        admin.setUsername(adminRegisterParam.getUsername());
         BeanUtil.copyProperties(adminRegisterParam, admin);
+        // 2.2 设置创建时间
+        admin.setCreateTime(new Date());
+        // 2.3 设置为启用状态
+        admin.setStatus(1);
         int i = adminMapper.insertSelective(admin);
         // 3 返回结果集
         return admin;
