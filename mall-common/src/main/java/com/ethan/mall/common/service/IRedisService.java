4566945bcefb5caacf98cddaa2885d4bc5a85616
HUIZHNEG940722 <zhenghui940722@outlook.com>
2022-01-06 08:19:22 +0800

    redis基础配置

---
+++ mall-common/src/main/java/com/ethan/mall/common/service/IRedisService.java
@@ -0,0 +1,9 @@
+package com.ethan.mall.common.service;
+
+/**
+ * @author ethan
+ * @Date 8:13 AM 2022/1/6
+ * @Description redis操作Service
+ */
+public interface IRedisService {
+}
---
+++ mall-common/src/main/java/com/ethan/mall/common/service/impl/RedisService.java
@@ -0,0 +1,11 @@
+package com.ethan.mall.common.service.impl;
+
+import com.ethan.mall.common.service.IRedisService;
+
+/**
+ * @author ethan
+ * @Date 8:13 AM 2022/1/6
+ * @Description redis操作类
+ */
+public class RedisService implements IRedisService {
+}
